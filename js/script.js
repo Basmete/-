4e7985ee1272753e56a902f4234a230d1e9a1d17
basmete <basmete@mail.ru>
2019-10-18 06:18:04 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let popup = document.getElementById('first-quiz');
+let popupToggle = document.getElementById('btn-diagnostic');
+
+popupToggle.onclick = function() {
+  popup.style.display="block";
+}
+
+let popup2 = document.getElementById('orderr');
+let popupToggle2 = document.getElementById('btn-contacts');
+
+popupToggle2.onclick = function() {
+  popup2.style.display="block";
+}
